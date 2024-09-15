12dac2fa44c339e84f05fd520390e02c307887d0
Adrian Stefan <77865941+stefan-code-js@users.noreply.github.com>
2024-09-15 16:42:16 +0200

    Update settings.js

---
+++ config/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+    botName: 'YourBotName',
+    defaultXPReward: 50,
+    defaultCreditsReward: 100,
+    maxMiningCooldown: 300,  // in seconds
+    maxFishingCooldown: 300,  // in seconds
+    maxLootingCooldown: 300,  // in seconds
+};
